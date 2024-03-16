498d9b6a3dec2ce5c6578b4bd4ff5c8d17be3bf9
Phakaphol <64467457+GodzK@users.noreply.github.com>
2024-03-16 12:24:06 +0700

    wrong path sorry add a path error

---
+++ my-app/app/not-found.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div>
+       <main className='not-found'>
+    <h1>Not Found</h1>
+    <p>Unfortunately, we could not fint  the request page or resource</p>
+    </main>
+    </div>
+  )
+}
+
+export default Notfound
--- my-app/lib/meals.js
+++ my-app/lib/meals.js
@@ -4,7 +4,7 @@ const db = sql('meals.db');
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve,1000));
 
-    throw new Error('loading meales');
+   
   return db.prepare('SELECT * FROM meals').all();
   
 }
